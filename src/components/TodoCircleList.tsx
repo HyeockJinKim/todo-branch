e108cff90c37c7eb08f00da0822d0ef7179ec8e3
HyeockJinKim <kherootz@gmail.com>
2020-05-28 21:18:48 +0900

    dock 사라지는 버그 수정

--- src/components/TodoCircleList.tsx
+++ src/components/TodoCircleList.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useBranchState, useTodoDispatch} from "../context/Todo.context";
+import ReactTooltip from "react-tooltip";
 
 function TodoCircleList() {
   const branches = useBranchState();
   const dispatch = useTodoDispatch();
   const cursor_css = {cursor: 'pointer'};
   const circle_location = (x: number) => x * 50 + 10;
+  useEffect(() => {
+    ReactTooltip.rebuild();
+  });
   return (
     <g className="todo-circle">
       {branches.map(branch => (
