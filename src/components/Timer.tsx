e63e0d62da1fab731450c7a6505594be26d3c959
HyeockJinKim <kherootz@gmail.com>
2020-06-01 00:01:23 +0900

    현재 시간이 보이도록 수정
    메인 화면에서 현재 시간이 보이도록 변경

---
+++ src/components/Timer.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+function Timer() {
+  const datetime_to_string = (d: Date) => {
+    if (d === null || d === undefined)
+      return '';
+    const _date = new Date(d);
+    return <div style={{marginTop: "3rem"}}>
+      <h2 style={{fontSize: "24px", margin: ".1rem auto", fontWeight: "bold"}}>{_date.getFullYear() + '년 ' + (_date.getMonth() + 1) + '월 ' + _date.getDate() + '일'}</h2>
+      <span style={{fontSize: "17px"}}>{_date.getHours() + '시 ' + _date.getMinutes() + '분 ' + _date.getSeconds() + '초'}</span>
+    </div>;
+  }
+
+  const [time, setTime] = useState(new Date());
+  setTimeout(() => setTime(new Date()), 1000);
+  return (
+    <div className="Timer">
+      {datetime_to_string(time)}
+    </div>
+  );
+}
+
+export default Timer;
