6bf80d91e1684de0f80d7ee6aeb32a41dc94314c
HyeockJinKim <kherootz@gmail.com>
2020-06-02 18:18:12 +0900

    Timer 초 제거
    rendering이 너무 많은 CPU를 사용해서
    분 단위로 갱신하도록 수정

--- src/components/branch/Timer.tsx
+++ src/components/branch/Timer.tsx
@@ -7,12 +7,12 @@ function Timer() {
     const _date = new Date(d);
     return <div style={{marginTop: "3rem"}}>
       <h2 style={{fontSize: "24px", margin: ".1rem auto", fontWeight: "bold"}}>{_date.getFullYear() + '년 ' + (_date.getMonth() + 1) + '월 ' + _date.getDate() + '일'}</h2>
-      <span style={{fontSize: "17px"}}>{_date.getHours() + '시 ' + _date.getMinutes() + '분 ' + _date.getSeconds() + '초'}</span>
+      <span style={{fontSize: "17px"}}>{_date.getHours() + '시 ' + _date.getMinutes() + '분'}</span>
     </div>;
   }
 
   const [time, setTime] = useState(new Date());
-  setTimeout(() => setTime(new Date()), 1000);
+  setTimeout(() => setTime(new Date()), 60000 - time.getSeconds()*1000);
   return (
     <div className="Timer">
       {datetime_to_string(time)}
